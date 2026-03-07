c60d2171da0813ea7fac0345730e5a7f2bea5a54
Avinash Jha <avinash2002a@gmail.com>
2026-03-07 06:54:11 +0530

    udpate: nvim diagnostics

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -13,16 +13,38 @@ vim.opt.relativenumber = true
 vim.o.cmdheight = 0
 -- vim.cmd.colorscheme "catppuccin"
 
+local icons = {
+  [vim.diagnostic.severity.ERROR] = " ",
+  [vim.diagnostic.severity.WARN]  = " ",
+  [vim.diagnostic.severity.INFO]  = " ",
+  [vim.diagnostic.severity.HINT]  = "󰌵 ",
+}
+
 vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN]  = " ",
+      [vim.diagnostic.severity.INFO]  = " ",
+      [vim.diagnostic.severity.HINT]  = "󰌵 ",
+    },
+  },
   virtual_text = {
     spacing = 4,
-    prefix = "●",
-    severity = { min = vim.diagnostic.severity.WARN },
+    source = "if_many",
+    format = function(d)
+      return (icons[d.severity] or "● ") .. d.message
+    end,
   },
   float = {
     border = "rounded",
-    source = "always",
+    source = "if_many",
     header = "",
-    prefix = "",
-  }
+    prefix = function(d)
+      return icons[d.severity] or "● ", ""
+    end,
+  },
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
 })
